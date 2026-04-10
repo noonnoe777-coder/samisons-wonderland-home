f2dce7998321303418d0812edd6af9b399526e7f
Noon Noe <nooninchai777@gmail.com>
2026-04-10 12:35:56 +0100

    Adjust mobile zoom

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,8 @@ export const metadata: Metadata = {
 
 export const viewport: Viewport = {
   width: "device-width",
-  initialScale: 1,
+  initialScale: 0.8,
+  maximumScale: 5,
   userScalable: true,
 };
 
